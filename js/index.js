b647c63ecc0fdb3ba7af131f64e3422059cacac7
Jude Mallett <43709074+judemallett@users.noreply.github.com>
2019-11-14 14:31:54 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,12 +8,11 @@ function buttonSubmit()
     var rand = random();
     $('#answer').text(rand);
     
-    navigator.notification.beep(1);
-    /*if(rand)
+    if(rand)
     {
         navigator.notification.beep(1);
     } else
     {
         navigator.notification.beep(2);
-    }*/
+    }
 }
